be84d643e75a2336619589d3646f12b9f1260131
osyra42 <22022820+osyra42@users.noreply.github.com>
2022-11-20 17:26:28 -0500

    update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,9 @@
-
+var character = document.getElementById("character");
+document.addEventListener("click",jump);
+function jump(){
+    character.classList.add("animate");
+    setTimeout(removeJump,300); //300ms = length of animation
+};
+function removeJump(){
+    character.classList.remove("animate");
+}
