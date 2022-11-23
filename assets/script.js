c22f4416982cc14982e529d5e214bef98a451460
osyra42 <22022820+osyra42@users.noreply.github.com>
2022-11-22 22:00:15 -0500

    Delete assets directory

--- assets/script.js
+++
@@ -1,9 +0,0 @@
-var character = document.getElementById("character");
-document.addEventListener("click",jump);
-function jump(){
-    character.classList.add("animate");
-    setTimeout(removeJump,300); //300ms = length of animation
-};
-function removeJump(){
-    character.classList.remove("animate");
-}
